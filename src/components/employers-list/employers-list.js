67be3d41ea8429c7d9ff6c4bfb1d825e88b63277
lexasby <alexey.aka.lexas@gmail.com>
2023-11-16 16:14:23 +0100

    add some events

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,11 +1,13 @@
 import React from "react";
 import EmployersListItem from "../employers-list-item/employers-list-item";
 import "./employers-list.css";
+
 const EmployersList = ({ data }) => {
 
   const elements = data.map((item) => {
+    const {id, ...itemProps} = item;
     return (
-      <EmployersListItem {...item}/>
+      <EmployersListItem key={id} {...itemProps}/>
     )
   });
 
